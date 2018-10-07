1a9b2f8cdfa90a609812535f8dca4758679718cc
Jonathan Ben-tzur <yoni@determined.ai>
2018-10-07 16:08:04 -0700

    Define custom error types for invalid id generation

---
+++ pkg/errors.go
@@ -0,0 +1,22 @@
+package pkg
+
+import "fmt"
+
+type SequenceUnavailable struct {
+	BucketID  uint64
+	Timestamp FlakeTime
+}
+
+func (err SequenceUnavailable) Error() string {
+	return fmt.Sprintf("maximum sequences hit for bucket %d: %s", err.BucketID, err.Timestamp)
+}
+
+type TimeMovedBack struct {
+	BucketID      uint64
+	LastTimestamp FlakeTime
+	Timestamp     FlakeTime
+}
+
+func (err TimeMovedBack) Error() string {
+	return fmt.Sprintf("time moved back for bucket %d: %s to %s", err.BucketID, err.LastTimestamp, err.Timestamp)
+}
