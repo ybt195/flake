a39f5fb89297eade9b827be182bc7844ed210900
Jonathan Ben-tzur <yoni@determined.ai>
2018-10-07 16:06:15 -0700

    Define basic flake id and time constructs

---
+++ pkg/id.go
@@ -0,0 +1,50 @@
+package pkg
+
+import (
+	"fmt"
+)
+
+const (
+	zeroID = FlakeID(0)
+
+	bucketBits    = 10
+	timestampBits = 42
+	sequenceBits  = 12
+
+	bucketLimit   = 1<<bucketBits - 1
+	sequenceLimit = 1<<sequenceBits - 1
+)
+
+type FlakeID uint64
+
+func (f FlakeID) HostID() uint64 {
+	return f.Uint64() >> (timestampBits + sequenceBits)
+}
+
+func (f FlakeID) FlakeTime() FlakeTime {
+	return FlakeTime(f.Uint64() << (bucketBits) >> (bucketBits + sequenceBits))
+}
+
+func (f FlakeID) Sequence() uint64 {
+	return f.Uint64() << (timestampBits + bucketBits) >> (timestampBits + bucketBits)
+}
+
+func (f FlakeID) String() string {
+	return fmt.Sprintf("{Host: %d, Time: %s, Sequence: %d}", f.HostID(), f.FlakeTime(), f.Sequence())
+}
+
+func (f FlakeID) Uint64() uint64 {
+	return uint64(f)
+}
+
+func (f FlakeID) Binary() string {
+	return fmt.Sprintf("%064b", f.Uint64())
+}
+
+func (f FlakeID) Octal() string {
+	return fmt.Sprintf("%o", f.Uint64())
+}
+
+func (f FlakeID) Hex() string {
+	return fmt.Sprintf("%x", f.Uint64())
+}
---
+++ pkg/time.go
@@ -0,0 +1,27 @@
+package pkg
+
+import (
+	"time"
+)
+
+const (
+	epoch = uint64(1262304000000)
+)
+
+type FlakeTime uint64
+
+func (f FlakeTime) Timestamp() time.Time {
+	return time.Unix(0, int64(uint64(f)+epoch)*int64(time.Millisecond)).In(time.UTC)
+}
+
+func (f FlakeTime) String() string {
+	return f.Timestamp().Format("2006-01-02T15:04:05.999Z07:00")
+}
+
+func now() uint64 {
+	return toMillis(time.Now()) - epoch
+}
+
+func toMillis(t time.Time) uint64 {
+	return uint64(t.UnixNano()) / uint64(time.Millisecond)
+}
